ba75ec8fe4079640467696ed46be68c3ac863f29
paulcardozo <40250579+paulcardozo@users.noreply.github.com>
2020-07-26 19:59:38 -0400

    Add files via upload

---
+++ 2patternMatch.sql
@@ -0,0 +1,16 @@
+﻿create or replace function busqueda(cadena varchar)
+	returns table(nombrecia char,
+	nombrecontacto char,
+	direccioncli char)
+	language plpgsql
+as
+$$
+begin
+	RETURN QUERY
+	select cl.nombrecia,cl.nombrecontacto,cl.direccioncli
+	from clientes as cl
+	where upper(cl.nombrecia) like concat('%',upper(cadena),'%')
+	or upper(cl.nombrecontacto) like concat('%',upper(cadena),'%')
+	or upper(cl.direccioncli) like concat('%',upper(cadena),'%');
+end;
+$$
